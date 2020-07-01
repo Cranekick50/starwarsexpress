bb03056f496511c5104ca92d2ea4ef485ec699ee
Cranekick50 <michael.srithapin@gmail.com>
2020-06-30 20:37:18 -0500

    updating exercise 3

--- server1.js
+++ server1.js
@@ -1,49 +1,17 @@
-// Dependencies
-// ===========================================================
-var express = require("express");
-
-var app = express();
-var PORT = process.env.PORT || 3000;
-
-// Data
-var characters = [{
-  routeName: "yoda",
-  name: "Yoda",
-  role: "Jedi Master",
-  age: 900,
-  forcePoints: 2000
-}, {
-  routeName: "darthmaul",
-  name: "Darth Maul",
-  role: "Sith Lord",
-  age: 200,
-  forcePoints: 1200
-}, {
-  routeName: "obiwankenobi",
-  name: "Obi Wan Kenobi",
-  role: "Jedi Knight",
-  age: 60,
-  forcePoints: 1350
-}];
-
-// Routes
-// ===========================================================
-app.get("/", function(req, res) {
-  res.send("Welcome to the Star Wars Page!");
-});
-
-app.get("/:character", function(req, res) {
-  var chosen = req.params.character;
-
-  // What does this log?
-  console.log(chosen);
-
-  res.end();
-});
-
-
-// Listener
-// ===========================================================
-app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
-});
+{
+  "name": "starwars-express",
+  "version": "1.0.0",
+  "description": "",
+  "main": "server3.js",
+  "dependencies": {
+    "express": "^4.16.3"
+  },
+  "devDependencies": {},
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1",
+    "start": "node server3.js",
+    "watch": "nodemon server3.js"
+  },
+  "author": "",
+  "license": "ISC"
+}
